c3f8fd6298519d045f2ce31fdb5e4c689997605a
Safal <safalpoudel808@gmail.com>
2025-05-30 11:39:47 +0545

    removed genai button from nav

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"578br58hHyvQNHY4CaFbXN5YPxVY4SJ5nHOEuMq1YEA=\"\n}"
---
+++ .next/static/chunks/app_[locale]_page_tsx_4c3fbe._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_[locale]_page_tsx_4c3fbe._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_[locale]_page_tsx_4c3fbe._.js",
+  "chunks": [
+    "static/chunks/_464f02._.js",
+    "static/chunks/node_modules_prism-react-renderer_dist_index_mjs_28522b._.js"
+  ],
+  "source": "dynamic"
+});
